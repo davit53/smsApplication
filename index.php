82df1d4b1c4d85cf21cfbc2e1f61187dfac20b54
Davit Najaryan <najaryan.d@gmail.com>
2022-08-26 18:36:31 -0400

    test v1.0

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>PHP SMS Website</title>
+  <title>PHP SMS Website 1</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
