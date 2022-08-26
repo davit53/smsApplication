595c205f241357f9b2673128602974d7fab03628
Edwin <104275615+EdwinNgui@users.noreply.github.com>
2022-08-26 18:33:30 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-test
+<?php
+if (isset($_POST['submit'])){
+    $mobile=$POST['mobile'];
+    $message=$_POST['message'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PHP SMS Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2 class="text-center">SMS Motivational Quotes</h2>
+  <form class="form-horizontal" method="post">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="mobile">Phone Number:</label>
+      <div class="col-sm-10">
+        <input type="number" class="form-control" id="email" placeholder="Enter phone number" name="mobile">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="message">Message:</label>
+      <div class="col-sm-10">          
+        <textarea name="message" class="form-control" placeholder="Enter message"></textarea>
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" name="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
